d081c514760824cf2b979199482e5ac883d7c420
JP Dillingham <jpdillingham@gmail.com>
2016-02-20 06:44:13 -0600

    Moved the ContractResolverType into ContractResolver.cs.  Eliminated Enumerations.cs

--- Core/Common/ContractResolver.cs
+++ Core/Common/ContractResolver.cs
@@ -8,6 +8,25 @@ using NLog;
 namespace Symbiote.Core
 {
     /// <summary>
+    /// Specifies how the array of properties passed to the constructor of the ContractResolver is to be used.
+    /// </summary>
+    public enum ContractResolverType
+    {
+        /// <summary>
+        /// The default type
+        /// </summary>
+        Undefined,
+        /// <summary>
+        /// Resolves the data contract using only the properties in accompanying list of properties
+        /// </summary>
+        OptIn,
+        /// <summary>
+        /// Resolves the data contract using all properties not included in the accompanying list of properties
+        /// </summary>
+        OptOut
+    }
+
+    /// <summary>
     /// The ContractResolver acts as a generic Data Contract Resolver, allowing for a list of ignored properties to be passed in on construction.
     /// </summary>
     class ContractResolver : DefaultContractResolver
