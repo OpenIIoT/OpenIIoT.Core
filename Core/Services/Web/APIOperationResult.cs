8507e5e977d6c5ae2e0a0edb3473291b0e29179b
JP Dillingham <jpdillingham@gmail.com>
2016-02-21 18:47:05 -0600

    Moved Services\Web\RequestInfo into ..\APIOperationResult.  Deleted RequestInfo.cs

--- Core/Services/Web/APIOperationResult.cs
+++ Core/Services/Web/APIOperationResult.cs
@@ -11,6 +11,30 @@ using System.Threading.Tasks;
 
 namespace Symbiote.Core.Services.Web
 {
+    public class RequestInfo
+    {
+        public string ShortGuid { get; private set; }
+        public string Route { get; private set; }
+        public string RemoteIP { get; private set; }
+
+        public RequestInfo(HttpRequestMessage request)
+        {
+            ShortGuid = Utility.ShortGuid();
+            Route = request.RequestUri.PathAndQuery;
+            RemoteIP = request.GetOwinContext().Request.RemoteIpAddress;
+        }
+
+        public override string ToString()
+        {
+            return "API Request [ID: " + ShortGuid + "]; Route: " + Route + "; Remote IP: " + RemoteIP;
+        }
+
+        public string ReturnString(HttpStatusCode returnCode)
+        {
+            return ToString() + " returned " + returnCode;
+        }
+    }
+
     class APIOperationResult
     {
         public HttpRequestMessage Request { get; private set; }
