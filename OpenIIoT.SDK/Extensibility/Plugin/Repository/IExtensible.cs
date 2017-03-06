6dfff798b68c6783944cf64a7b2de96e002c4f8c
JP Dillingham <jpdillingham@gmail.com>
2017-03-05 18:42:58 -0600

    Started fleshing out the design of the Repository plugin type

---
+++ OpenIIoT.SDK/Extensibility/Plugin/Repository/IExtensible.cs
@@ -0,0 +1,16 @@
+﻿using OpenIIoT.SDK.Extensibility.Plugin;
+using Utility.OperationResult;
+
+namespace OpenIIoT.SDK.Extensibility.Plugin.Repository
+{
+    public interface IExtensible
+    {
+        Result CreateTable();
+
+        Result DropTable();
+
+        Result CreateView();
+
+        Result DropView();
+    }
+}
---
+++ OpenIIoT.SDK/Extensibility/Plugin/Repository/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using OpenIIoT.SDK.Extensibility.Plugin;
+using Utility.OperationResult;
+
+namespace OpenIIoT.SDK.Extensibility.Plugin.Repository
+{
+    public interface IRepository : IPluginInstance
+    {
+    }
+}
