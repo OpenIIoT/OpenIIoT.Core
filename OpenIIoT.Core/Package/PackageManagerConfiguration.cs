9afb4d9d71e79ca8fd207038a5dd9da22d8ea55e
JP Dillingham <jpdillingham@gmail.com>
2017-03-11 15:37:39 -0600

    Eliminated Package.Configuration namespace

--- OpenIIoT.Core/Package/PackageManagerConfiguration.cs
+++ OpenIIoT.Core/Package/PackageManagerConfiguration.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace OpenIIoT.Core.Package.Configuration
+namespace OpenIIoT.Core.Package
 {
     /// <summary>
     ///     A class representing the configuration items for the Model Manager.
