b9bc84528aeb0955a16e154748a4d2e32d7c3789
JP Dillingham <jpdillingham@gmail.com>
2017-04-15 20:09:11 -0500

    Added PackageManifestBuilder

---
+++ OpenIIoT.SDK/Package/Manifest/IPackageManifestBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenIIoT.SDK.Package.Manifest
+{
+    public interface IPackageManifestBuilder
+    {
+        #region Public Properties
+
+        PackageManifest Manifest { get; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        PackageManifestBuilder AddFile(IPackageManifestFile file);
+
+        PackageManifestBuilder Copyright(string copyright);
+
+        PackageManifestBuilder Description(string description);
+
+        PackageManifestBuilder Files(List<IPackageManifestFile> files);
+
+        PackageManifestBuilder License(string license);
+
+        PackageManifestBuilder Namespace(string ns);
+
+        PackageManifestBuilder Publisher(string publisher);
+
+        PackageManifestBuilder Signature(IPackageManifestSignature signature);
+
+        PackageManifestBuilder Title(string title);
+
+        PackageManifestBuilder Url(string url);
+
+        PackageManifestBuilder Version(string version);
+
+        PackageManifestBuilder BuildDefault(IList<IPackageManifestFile> files);
+
+        PackageManifestBuilder ClearFiles();
+
+        PackageManifestBuilder RemoveFile(IPackageManifestFile file);
+
+        #endregion Public Methods
+    }
+}
---
+++ OpenIIoT.SDK/Package/Manifest/PackageManifestBuilder.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenIIoT.SDK.Package.Manifest
+{
+    public class PackageManifestBuilder : IPackageManifestBuilder
+    {
+        #region Public Constructors
+
+        public PackageManifestBuilder(PackageManifest manifest = default(PackageManifest))
+        {
+            if (manifest == default(PackageManifest))
+            {
+                Manifest = new PackageManifest();
+            }
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public PackageManifest Manifest { get; private set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public PackageManifestBuilder BuildDefault(IList<IPackageManifestFile> files = default(IList<IPackageManifestFile>))
+        {
+            this.Title("DefaultPlugin")
+                .Version("1.0.0")
+                .Namespace("OpenIIoT.Plugin")
+                .Description("Default plugin.")
+                .Publisher("OpenIIoT Team")
+                .Copyright("Copyright (c) " + DateTime.Now.Year + " OpenIIoT")
+                .License("AGPLv3")
+                .Url("http://github.com/openiiot");
+
+            if (files != default(IList<IPackageManifestFile>))
+            {
+                foreach (IPackageManifestFile file in files)
+                {
+                    this.AddFile(file);
+                }
+            }
+
+            return this;
+        }
+
+        public PackageManifestBuilder AddFile(IPackageManifestFile file)
+        {
+            if (Manifest.Files == default(IList<IPackageManifestFile>))
+            {
+                Manifest.Files = new List<IPackageManifestFile>();
+            }
+
+            Manifest.Files.Add(file);
+
+            return this;
+        }
+
+        public PackageManifestBuilder RemoveFile(IPackageManifestFile file)
+        {
+            if (Manifest.Files != default(IList<IPackageManifestFile>) && Manifest.Files.Contains(file))
+            {
+                Manifest.Files.Remove(file);
+            }
+
+            return this;
+        }
+
+        public PackageManifestBuilder ClearFiles()
+        {
+            if (Manifest.Files != default(IList<IPackageManifestFile>))
+            {
+                Manifest.Files.Clear();
+            }
+
+            return this;
+        }
+
+        public PackageManifestBuilder Copyright(string copyright)
+        {
+            Manifest.Copyright = copyright;
+            return this;
+        }
+
+        public PackageManifestBuilder Description(string description)
+        {
+            Manifest.Description = description;
+            return this;
+        }
+
+        public PackageManifestBuilder Files(List<IPackageManifestFile> files)
+        {
+            Manifest.Files = files;
+            return this;
+        }
+
+        public PackageManifestBuilder License(string license)
+        {
+            Manifest.License = license;
+            return this;
+        }
+
+        public PackageManifestBuilder Namespace(string ns)
+        {
+            Manifest.Namespace = ns;
+            return this;
+        }
+
+        public PackageManifestBuilder Publisher(string publisher)
+        {
+            Manifest.Publisher = publisher;
+            return this;
+        }
+
+        public PackageManifestBuilder Signature(IPackageManifestSignature signature)
+        {
+            Manifest.Signature = signature;
+            return this;
+        }
+
+        public PackageManifestBuilder Title(string title)
+        {
+            Manifest.Title = title;
+            return this;
+        }
+
+        public PackageManifestBuilder Url(string url)
+        {
+            Manifest.Url = url;
+            return this;
+        }
+
+        public PackageManifestBuilder Version(string version)
+        {
+            Manifest.Version = version;
+            return this;
+        }
+
+        #endregion Public Methods
+    }
+}
