dc8adaf00943ddd46586d0cf7f344df77caeb216
JP Dillingham <jpdillingham@gmail.com>
2017-04-24 20:22:36 -0500

    Removed header from AssemblyInfo.cs across all projects

--- OpenIIoT.SDK.Tests/Properties/AssemblyInfo.cs
+++ OpenIIoT.SDK.Tests/Properties/AssemblyInfo.cs
@@ -1,45 +1,4 @@
-﻿/*
-      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀  ▀  ▀      ▀▀
-      █
-      █     ▄████████                                                                      ▄█
-      █     ███    ███                                                                    ███
-      █     ███    ███   ▄█████   ▄█████    ▄█████    ▄▄██▄▄▄  ▀██████▄   █       ▄█   ▄  ███▌ ██▄▄▄▄     ▄█████  ██████
-      █     ███    ███   ██  ▀    ██  ▀    ██   █   ▄█▀▀██▀▀█▄   ██   ██ ██       ██   █▄ ███▌ ██▀▀▀█▄   ██   ▀█ ██    ██
-      █   ▀███████████   ██       ██      ▄██▄▄     ██  ██  ██  ▄██▄▄█▀  ██       ▀▀▀▀▀██ ███▌ ██   ██  ▄██▄▄    ██    ██
-      █     ███    ███ ▀███████ ▀███████ ▀▀██▀▀     ██  ██  ██ ▀▀██▀▀█▄  ██       ▄█   ██ ███  ██   ██ ▀▀██▀▀    ██    ██
-      █     ███    ███    ▄  ██    ▄  ██   ██   █   ██  ██  ██   ██   ██ ██▌    ▄ ██   ██ ███  ██   ██   ██      ██    ██
-      █     ███    █▀   ▄████▀   ▄████▀    ███████   █  ██  █  ▄██████▀  ████▄▄██  █████  █▀    █   █    ██       ██████
-      █
- ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄  ▄▄ ▄▄   ▄▄▄▄ ▄▄     ▄▄     ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄ ▄
- █████████████████████████████████████████████████████████████ ███████████████ ██  ██ ██   ████ ██     ██     ████████████████ █ █
-      ▄
-      █  Information about the assembly.
-      █
-      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀ ▀ ▀▀▀     ▀▀               ▀
-      █  The GNU Affero General Public License (GNU AGPL)
-      █
-      █  Copyright (C) 2016-2017 JP Dillingham (jp@dillingham.ws)
-      █
-      █  This program is free software: you can redistribute it and/or modify
-      █  it under the terms of the GNU Affero General Public License as published by
-      █  the Free Software Foundation, either version 3 of the License, or
-      █  (at your option) any later version.
-      █
-      █  This program is distributed in the hope that it will be useful,
-      █  but WITHOUT ANY WARRANTY; without even the implied warranty of
-      █  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-      █  GNU Affero General Public License for more details.
-      █
-      █  You should have received a copy of the GNU Affero General Public License
-      █  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-      █
-      ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  ▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀██
-                                                                                                   ██
-                                                                                               ▀█▄ ██ ▄█▀
-                                                                                                 ▀████▀
-                                                                                                   ▀▀                            */
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("OpenIIoT.SDK.Tests")]
@@ -52,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c01490c0-a243-4fb6-b6ef-a5fb534d20f0")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
