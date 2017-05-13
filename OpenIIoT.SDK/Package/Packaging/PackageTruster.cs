cefe31f0f3fd4b7566f8768fcfa8485595c8a33c
JP Dillingham <jpdillingham@gmail.com>
2017-05-13 16:16:36 -0500

    Implemented UpdatePackageManifest in PackageTruster.cs

--- OpenIIoT.SDK/Package/Packaging/PackageTruster.cs
+++ OpenIIoT.SDK/Package/Packaging/PackageTruster.cs
@@ -26,23 +26,59 @@ namespace OpenIIoT.SDK.Package.Packaging
             string privateKey = File.ReadAllText(privateKeyFile);
 
             byte[] trustBytes = PGPSignature.Sign(digestBytes, privateKey, passphrase);
-
             string trust = Encoding.ASCII.GetString(trustBytes);
 
             manifest.Signature.Trust = trust;
 
-            ZipArchive package = ZipFile.Open(packageFile, ZipArchiveMode.Update);
-
-            package.GetEntry(Package.Constants.ManifestFilename).Delete();
-
-            File.WriteAllText(@"c:\pkg\manifest.json", manifest.ToJson());
-            package.CreateEntryFromFile(@"c:\pkg\manifest.json", Package.Constants.ManifestFilename);
-            package.Dispose();
+            UpdatePackageManifest(packageFile, manifest);
+            OnUpdated(" √ Manifest updated successfully.");
         }
 
         #endregion Public Methods
 
         #region Private Methods
+
+        private static void UpdatePackageManifest(string packageFile, PackageManifest manifest)
+        {
+            // looks like: temp\OpenIIoT.SDK\<Guid>\
+            string tempDirectory = Path.Combine(Path.GetTempPath(), System.Reflection.Assembly.GetEntryAssembly().GetName().Name, Guid.NewGuid().ToString());
+            string tempFile = Path.Combine(tempDirectory, Package.Constants.ManifestFilename);
+
+            OnUpdated($"Updating Manifest in Package '{Path.GetFileName(packageFile)}'...");
+
+            try
+            {
+                OnUpdated($"Writing Manifest to temp file '{tempFile}'...");
+                File.WriteAllText(tempFile, manifest.ToJson());
+                OnUpdated(" √ Manifest file written successfully.");
+
+                OnUpdated($"Opening Package file '{Path.GetFileName(packageFile)}'...");
+                ZipArchive package = ZipFile.Open(packageFile, ZipArchiveMode.Update);
+                OnUpdated(" √ Package file opened successfully.");
+
+                OnUpdated("Deleting existing Manifest file...");
+                package.GetEntry(Package.Constants.ManifestFilename).Delete();
+                OnUpdated(" √ Deleted existing Manifest file successfully.");
+
+                OnUpdated("Adding updated Manfiest file...");
+                package.CreateEntryFromFile(tempFile, Package.Constants.ManifestFilename);
+                OnUpdated(" √ Manifest updated successfully.");
+
+                OnUpdated("Saving Package...");
+                package.Dispose();
+                OnUpdated(" √ Package saved successfully.");
+            }
+            catch (Exception ex)
+            {
+                OnUpdated($"Error updating Manifest in Package '{Path.GetFileName(packageFile)}: {ex.Message}'");
+            }
+            finally
+            {
+                OnUpdated("Deleting temporary files...");
+                Directory.Delete(tempDirectory, true);
+                OnUpdated(" √ Temporary files deleted successfully.");
+            }
+        }
 
         private static void OnUpdated(string message)
         {
