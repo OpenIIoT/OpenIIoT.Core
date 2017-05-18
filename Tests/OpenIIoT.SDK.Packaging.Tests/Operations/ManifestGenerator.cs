c466e4085a89f84f561c19bd07da1da5553fd1ca
JP Dillingham <jpdillingham@gmail.com>
2017-05-17 19:07:23 -0500

    Began writing tests for SDK.Packaging

---
+++ Tests/OpenIIoT.SDK.Packaging.Tests/Operations/ManifestGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xunit;
+
+namespace OpenIIoT.SDK.Packaging.Tests.Operations
+{
+    public class ManifestGenerator
+    {
+        #region Public Constructors
+
+        public ManifestGenerator()
+        {
+            generator = new Packaging.Operations.ManifestGenerator();
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public Packaging.Operations.ManifestGenerator generator { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        [Fact]
+        public void GenerateBlankDirectory()
+        {
+            Exception ex = Record.Exception(() => generator.GenerateManifest(string.Empty));
+
+            Assert.NotNull(ex);
+            Assert.IsType<ArgumentException>(ex);
+        }
+
+        [Fact]
+        public void GenerateNullDirectory()
+        {
+            Exception ex = Record.Exception(() => generator.GenerateManifest(null));
+
+            Assert.NotNull(ex);
+            Assert.IsType<ArgumentException>(ex);
+        }
+
+        #endregion Public Methods
+    }
+}
