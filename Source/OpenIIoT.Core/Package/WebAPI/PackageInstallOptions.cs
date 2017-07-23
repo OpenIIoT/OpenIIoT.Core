754de7ff7072cd302c0e5f3639014b7e64fa9e33
JP Dillingham <jpdillingham@gmail.com>
2017-07-22 20:14:14 -0500

    Added reference to System.ComponentModel.DataAnnotations

--- Source/OpenIIoT.Core/Package/WebAPI/PackageInstallOptions.cs
+++ Source/OpenIIoT.Core/Package/WebAPI/PackageInstallOptions.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Converters;
 using OpenIIoT.SDK.Package;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
