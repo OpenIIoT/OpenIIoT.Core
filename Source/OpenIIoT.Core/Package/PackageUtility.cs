638ad09f48816e07e5745254bcce9190aada933a
JP Dillingham <jpdillingham@gmail.com>
2017-08-01 18:47:30 -0500

    Made Read() in PackageUtility public again.

--- Source/OpenIIoT.Core/Package/PackageUtility.cs
+++ Source/OpenIIoT.Core/Package/PackageUtility.cs
@@ -110,7 +110,7 @@ namespace OpenIIoT.Core.Package
 
             if (retVal.ResultCode != ResultCode.Failure)
             {
-                IResult<IPackage> readResult = ReadPackage(tempFile);
+                IResult<IPackage> readResult = Read(tempFile);
 
                 retVal.Incorporate(readResult);
 
@@ -191,6 +191,42 @@ namespace OpenIIoT.Core.Package
         }
 
         /// <summary>
+        ///     Reads the specified file and, if it is a valid <see cref="Package"/>, returns an <see cref="IPackage"/> instance
+        ///     from the contents.
+        /// </summary>
+        /// <param name="fileName">The filename of the file to read.</param>
+        /// <returns>A Result containing the result of the operation and the created IPackage instance.</returns>
+        public IResult<IPackage> Read(string fileName)
+        {
+            logger.EnterMethod(true);
+            IResult<IPackage> retVal = new Result<IPackage>();
+
+            logger.Debug($"Reading Package '{fileName}'...");
+
+            ManifestExtractor extractor = new ManifestExtractor();
+            extractor.Updated += (sender, e) => logger.Debug(e.Message);
+
+            PackageManifest manifest;
+
+            try
+            {
+                manifest = extractor.ExtractManifest(fileName);
+
+                retVal.ReturnValue = GetPackage(fileName, manifest);
+            }
+            catch (Exception ex)
+            {
+                retVal.AddError(ex.Message);
+                retVal.AddError($"Unable to read Package '{Path.GetFileName(fileName)}'.");
+            }
+
+            retVal.LogResult(logger.Debug);
+            logger.ExitMethod();
+
+            return retVal;
+        }
+
+        /// <summary>
         ///     Scans the Packages directory and generates a list of found <see cref="IPackage"/> instances.
         /// </summary>
         /// <returns>A Result containing the result of the operation and the list of found IPackage instances.</returns>
@@ -214,7 +250,7 @@ namespace OpenIIoT.Core.Package
             {
                 foreach (string file in fileListResult.ReturnValue)
                 {
-                    IResult<IPackage> readResult = ReadPackage(file);
+                    IResult<IPackage> readResult = Read(file);
 
                     if (readResult.ResultCode != ResultCode.Failure)
                     {
@@ -266,42 +302,6 @@ namespace OpenIIoT.Core.Package
             }
 
             return Path.Combine(Platform.Directories.Packages, filename);
-        }
-
-        /// <summary>
-        ///     Reads the specified file and, if it is a valid <see cref="Package"/>, returns an <see cref="IPackage"/> instance
-        ///     from the contents.
-        /// </summary>
-        /// <param name="fileName">The filename of the file to read.</param>
-        /// <returns>A Result containing the result of the operation and the created IPackage instance.</returns>
-        private IResult<IPackage> ReadPackage(string fileName)
-        {
-            logger.EnterMethod(true);
-            IResult<IPackage> retVal = new Result<IPackage>();
-
-            logger.Debug($"Reading Package '{fileName}'...");
-
-            ManifestExtractor extractor = new ManifestExtractor();
-            extractor.Updated += (sender, e) => logger.Debug(e.Message);
-
-            PackageManifest manifest;
-
-            try
-            {
-                manifest = extractor.ExtractManifest(fileName);
-
-                retVal.ReturnValue = GetPackage(fileName, manifest);
-            }
-            catch (Exception ex)
-            {
-                retVal.AddError(ex.Message);
-                retVal.AddError($"Unable to read Package '{Path.GetFileName(fileName)}'.");
-            }
-
-            retVal.LogResult(logger.Debug);
-            logger.ExitMethod();
-
-            return retVal;
         }
 
         #endregion Private Methods
