a0bcf5481729af369ee5953b4a6cfb1311b726c6
JP Dillingham <jpdillingham@gmail.com>
2017-08-13 18:26:00 -0500

    Stubbed Security namespace

---
+++ OpenIIoT.Core/Security/WebAPI/SecurityController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using OpenIIoT.Core.Service.WebAPI;
+using OpenIIoT.SDK;
+
+namespace OpenIIoT.Core.Security.WebAPI
+{
+    /// <summary>
+    ///     API methods for application security.
+    /// </summary>
+    [RoutePrefix("api")]
+    public class SecurityController : ApiBaseController
+    {
+        #region Variables
+
+        /// <summary>
+        ///     The ApplicationManager for the application.
+        /// </summary>
+        private IApplicationManager manager = ApplicationManager.GetInstance();
+
+        #endregion Variables
+
+        #region Instance Methods
+
+        [HttpPost]
+        [Route("v1/security/login/{user}/{password}")]
+        public async Task<HttpResponseMessage> Login(string user, string password)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        #endregion Instance Methods
+    }
+}
