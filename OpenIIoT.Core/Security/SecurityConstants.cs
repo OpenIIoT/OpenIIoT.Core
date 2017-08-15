854dd69df540ace11d2e95dd93f7febf6a135fc0
JP Dillingham <jpdillingham@gmail.com>
2017-08-14 22:31:13 -0500

    Added SecurityConstants

---
+++ OpenIIoT.Core/Security/SecurityConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenIIoT.Core.Security
+{
+    public static class SecurityConstants
+    {
+        #region Public Fields
+
+        public const string AdministrativeRole = "Administrator";
+        public const string DefaultPassword = "C7AD44CBAD762A5DA0A452F9E854FDC1E0E7A52A38015F23F3EAB1D80B931DD472634DFAC71CD34EBC35D16AB7FB8A90C81F975113D6C7538DC69DD8DE9077EC";
+        public const string DefaultUser = "admin";
+        public static readonly string[] DefaultRoles = new string[] { "Public" };
+
+        #endregion Public Fields
+    }
+}
