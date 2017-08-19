8e3813cb83277ca9e549651931c031b8dbfb2430
JP Dillingham <jpdillingham@gmail.com>
2017-08-18 21:04:54 -0500

    Removed unused usings

--- OpenIIoT.Core/Security/User.cs
+++ OpenIIoT.Core/Security/User.cs
@@ -1,8 +1,4 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace OpenIIoT.Core.Security
+﻿namespace OpenIIoT.Core.Security
 {
     public class User
     {
