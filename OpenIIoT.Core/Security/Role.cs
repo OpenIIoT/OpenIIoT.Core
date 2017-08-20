8d0d0922568b914f186d5ad2204f2a99e0ba47fd
JP Dillingham <jpdillingham@gmail.com>
2017-08-19 22:03:06 -0500

    Documented Role

--- OpenIIoT.Core/Security/Role.cs
+++ OpenIIoT.Core/Security/Role.cs
@@ -1,10 +1,69 @@
-﻿namespace OpenIIoT.Core.Security
+﻿/*
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀  ▀  ▀      ▀▀
+      █
+      █      ▄████████
+      █     ███    ███
+      █    ▄███▄▄▄▄██▀  ██████   █          ▄█████
+      █   ▀▀███▀▀▀▀▀   ██    ██ ██         ██   █
+      █   ▀███████████ ██    ██ ██        ▄██▄▄
+      █     ███    ███ ██    ██ ██       ▀▀██▀▀
+      █     ███    ███ ██    ██ ██▌    ▄   ██   █
+      █     ███    ███  ██████  ████▄▄██   ███████
+      █
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄  ▄▄ ▄▄   ▄▄▄▄ ▄▄     ▄▄     ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄ ▄
+ █████████████████████████████████████████████████████████████ ███████████████ ██  ██ ██   ████ ██     ██     ████████████████ █ █
+      ▄
+      █  Enumeration of the built-in User Roles.
+      █
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀ ▀ ▀▀▀     ▀▀               ▀
+      █  The GNU Affero General Public License (GNU AGPL)
+      █
+      █  Copyright (C) 2016-2017 JP Dillingham (jp@dillingham.ws)
+      █
+      █  This program is free software: you can redistribute it and/or modify
+      █  it under the terms of the GNU Affero General Public License as published by
+      █  the Free Software Foundation, either version 3 of the License, or
+      █  (at your option) any later version.
+      █
+      █  This program is distributed in the hope that it will be useful,
+      █  but WITHOUT ANY WARRANTY; without even the implied warranty of
+      █  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      █  GNU Affero General Public License for more details.
+      █
+      █  You should have received a copy of the GNU Affero General Public License
+      █  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+      █
+      ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  ▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀██
+                                                                                                   ██
+                                                                                               ▀█▄ ██ ▄█▀
+                                                                                                 ▀████▀
+                                                                                                   ▀▀                            */
+
+namespace OpenIIoT.Core.Security
 {
+    /// <summary>
+    ///     Enumeration of the built-in User Roles.
+    /// </summary>
     public enum Role
     {
+        /// <summary>
+        ///     The default/uninitialized Role.
+        /// </summary>
         NotSpecified = 0,
+
+        /// <summary>
+        ///     The User may read data.
+        /// </summary>
         Reader = 1,
+
+        /// <summary>
+        ///     The User may read and write data.
+        /// </summary>
         ReadWriter = 2,
+
+        /// <summary>
+        ///     The User has full administrative control over data and configuration.
+        /// </summary>
         Administrator = 3
     }
 }
