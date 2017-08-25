7a0ef6b1580edc40045aeca94ca33fad7b1b4378
JP Dillingham <jpdillingham@gmail.com>
2017-08-24 21:09:26 -0500

    Added owin.Security package

--- OpenIIoT.Core.Tests/Security/SessionEventArgs.cs
+++ OpenIIoT.Core.Tests/Security/SessionEventArgs.cs
@@ -56,6 +56,7 @@
                                                                                                ▀█▄ ██ ▄█▀
                                                                                                  ▀████▀
                                                                                                    ▀▀                            */
+using Microsoft.Owin.Security;
 using Xunit;
 
 namespace OpenIIoT.Core.Tests.Security
