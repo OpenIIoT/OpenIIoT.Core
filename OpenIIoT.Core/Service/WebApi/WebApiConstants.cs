cde98d8093eaae94227237b340da173cd16943cd
JP Dillingham <jpdillingham@gmail.com>
2017-09-10 17:46:50 -0500

    Tweaked WebApi constants

--- OpenIIoT.Core/Service/WebApi/WebApiConstants.cs
+++ OpenIIoT.Core/Service/WebApi/WebApiConstants.cs
@@ -6,12 +6,14 @@
 
         public const string ApiKeyHeaderName = "X-ApiKey";
         public const string ApiRoutePrefix = "api";
+        public const string AssetPath = "assets";
         public const string HelpRoutePrefix = "help";
         public const string LoginRoutePrefix = "login";
+        public const string ModulePath = "node_modules";
         public const string NotFoundRoutePrefix = "404";
         public const string SessionTokenCookieName = "Session-Token";
         public const string SignalRRoutePrefix = "signalr";
-        public static readonly string[] AnonymousRoutes = { ApiRoutePrefix, SignalRRoutePrefix, HelpRoutePrefix, LoginRoutePrefix, HelpRoutePrefix, NotFoundRoutePrefix, "assets", "node_modules" };
+        public static readonly string[] AnonymousRoutes = { ApiRoutePrefix, SignalRRoutePrefix, HelpRoutePrefix, LoginRoutePrefix, HelpRoutePrefix, NotFoundRoutePrefix, AssetPath, ModulePath };
         public static readonly string[] RedirectSuppressedRoutes = { ApiRoutePrefix, SignalRRoutePrefix };
 
         #endregion Public Fields
