a787447a9521f4eb2d7170642554fcdabf826e06
JP Dillingham <jpdillingham@gmail.com>
2017-10-04 19:06:20 -0500

    Added documentation to ValidateModelAttribute

--- OpenIIoT.Core/Service/WebApi/ModelValidation/ValidateModelAttribute.cs
+++ OpenIIoT.Core/Service/WebApi/ModelValidation/ValidateModelAttribute.cs
@@ -1,24 +1,77 @@
-﻿using OpenIIoT.SDK.Common;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http.Controllers;
-using System.Web.Http.Filters;
-using System.Web.Http.ModelBinding;
+﻿/*
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀  ▀  ▀      ▀▀
+      █
+      █    ▄█    █▄                                                                   ▄▄▄▄███▄▄▄▄
+      █   ███    ███                                                                ▄██▀▀▀███▀▀▀██▄
+      █   ███    ███   ▄█████   █        █  ██████▄    ▄█████      ██       ▄█████  ███   ███   ███  ██████  ██████▄     ▄█████  █
+      █   ███    ███   ██   ██ ██       ██  ██   ▀██   ██   ██ ▀███████▄   ██   █   ███   ███   ███ ██    ██ ██   ▀██   ██   █  ██
+      █   ███    ███   ██   ██ ██       ██▌ ██    ██   ██   ██     ██  ▀  ▄██▄▄     ███   ███   ███ ██    ██ ██    ██  ▄██▄▄    ██
+      █   ███    ███ ▀████████ ██       ██  ██    ██ ▀████████     ██    ▀▀██▀▀     ███   ███   ███ ██    ██ ██    ██ ▀▀██▀▀    ██
+      █    ██▄  ▄██    ██   ██ ██▌    ▄ ██  ██   ▄██   ██   ██     ██      ██   █   ███   ███   ███ ██    ██ ██   ▄██   ██   █  ██▌    ▄
+      █     ▀████▀     ██   █▀ ████▄▄██ █   ██████▀    ██   █▀    ▄██▀     ███████   ▀█   ███   █▀   ██████  ██████▀    ███████ ████▄▄██
+      █
+      █     ▄████████
+      █     ███    ███
+      █     ███    ███     ██        ██       █████  █  ▀██████▄  ██   █      ██       ▄█████
+      █     ███    ███ ▀███████▄ ▀███████▄   ██  ██ ██    ██   ██ ██   ██ ▀███████▄   ██   █
+      █   ▀███████████     ██  ▀     ██  ▀  ▄██▄▄█▀ ██▌  ▄██▄▄█▀  ██   ██     ██  ▀  ▄██▄▄
+      █     ███    ███     ██        ██    ▀███████ ██  ▀▀██▀▀█▄  ██   ██     ██    ▀▀██▀▀
+      █     ███    ███     ██        ██      ██  ██ ██    ██   ██ ██   ██     ██      ██   █
+      █     ███    █▀     ▄██▀      ▄██▀     ██  ██ █   ▄██████▀  ██████     ▄██▀     ███████
+      █
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄  ▄▄ ▄▄   ▄▄▄▄ ▄▄     ▄▄     ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄ ▄
+ █████████████████████████████████████████████████████████████ ███████████████ ██  ██ ██   ████ ██     ██     ████████████████ █ █
+      ▄
+      █  Validates model parameters of WebApi controllers.
+      █
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀ ▀ ▀▀▀     ▀▀               ▀
+      █  The GNU Affero General Public License (GNU AGPL)
+      █
+      █  Copyright (C) 2016-2017 JP Dillingham (jp@dillingham.ws)
+      █
+      █  This program is free software: you can redistribute it and/or modify
+      █  it under the terms of the GNU Affero General Public License as published by
+      █  the Free Software Foundation, either version 3 of the License, or
+      █  (at your option) any later version.
+      █
+      █  This program is distributed in the hope that it will be useful,
+      █  but WITHOUT ANY WARRANTY; without even the implied warranty of
+      █  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      █  GNU Affero General Public License for more details.
+      █
+      █  You should have received a copy of the GNU Affero General Public License
+      █  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+      █
+      ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  ▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀██
+                                                                                                   ██
+                                                                                               ▀█▄ ██ ▄█▀
+                                                                                                 ▀████▀
+                                                                                                   ▀▀                            */
 
 namespace OpenIIoT.Core.Service.WebApi.ModelValidation
 {
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Filters;
+    using System.Web.Http.ModelBinding;
+
+    /// <summary>
+    ///     Validates model parameters of WebApi controllers.
+    /// </summary>
     public class ValidateModelAttribute : ActionFilterAttribute
     {
         #region Public Methods
 
+        /// <summary>
+        ///     Invoked prior to the execution of the WebApi controller.
+        /// </summary>
+        /// <param name="actionContext">The context in which the controller has been invoked.</param>
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
             if (!actionContext.ModelState.IsValid)
             {
-                actionContext.Response = actionContext.Request.CreateResponse(
-                    HttpStatusCode.BadRequest, GetResult(actionContext.ModelState));
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.BadRequest, GetResult(actionContext.ModelState));
             }
         }
 
@@ -26,10 +79,15 @@ namespace OpenIIoT.Core.Service.WebApi.ModelValidation
 
         #region Private Methods
 
+        /// <summary>
+        ///     Transforms the specified <see cref="ModelStateDictionary"/><paramref name="state"/> into a strongly typed data
+        ///     transfer object containing the same information.
+        /// </summary>
+        /// <param name="state">The <see cref="ModelStateDictionary"/> to transform.</param>
+        /// <returns>The transformed model state dictionary.</returns>
         private ModelValidationResult GetResult(ModelStateDictionary state)
         {
-            ModelValidationResult retVal = new ModelValidationResult();
-            retVal.Message = "The request data is invalid.";
+            ModelValidationResult retVal = new ModelValidationResult() { Message = "The request data is invalid." };
 
             foreach (string key in state.Keys)
             {
