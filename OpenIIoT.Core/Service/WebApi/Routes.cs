9265da10926b026a91e178d9a5c39b0c65b8e5f7
JP Dillingham <jpdillingham@gmail.com>
2017-10-08 09:28:14 -0500

    Added documentation for Routes/IRoutes

--- OpenIIoT.Core/Service/WebApi/Routes.cs
+++ OpenIIoT.Core/Service/WebApi/Routes.cs
@@ -1,16 +1,61 @@
-﻿using OpenIIoT.SDK.Service.WebApi;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀  ▀  ▀      ▀▀
+      █
+      █      ▄████████
+      █     ███    ███
+      █    ▄███▄▄▄▄██▀  ██████  ██   █      ██       ▄█████   ▄█████
+      █   ▀▀███▀▀▀▀▀   ██    ██ ██   ██ ▀███████▄   ██   █    ██  ▀
+      █   ▀███████████ ██    ██ ██   ██     ██  ▀  ▄██▄▄      ██
+      █     ███    ███ ██    ██ ██   ██     ██    ▀▀██▀▀    ▀███████
+      █     ███    ███ ██    ██ ██   ██     ██      ██   █     ▄  ██
+      █     ███    ███  ██████  ██████     ▄██▀     ███████  ▄████▀
+      █
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄  ▄▄ ▄▄   ▄▄▄▄ ▄▄     ▄▄     ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄ ▄
+ █████████████████████████████████████████████████████████████ ███████████████ ██  ██ ██   ████ ██     ██     ████████████████ █ █
+      ▄
+      █  Web Routes for the Web Service.
+      █
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀ ▀ ▀▀▀     ▀▀               ▀
+      █  The GNU Affero General Public License (GNU AGPL)
+      █
+      █  Copyright (C) 2016-2017 JP Dillingham (jp@dillingham.ws)
+      █
+      █  This program is free software: you can redistribute it and/or modify
+      █  it under the terms of the GNU Affero General Public License as published by
+      █  the Free Software Foundation, either version 3 of the License, or
+      █  (at your option) any later version.
+      █
+      █  This program is distributed in the hope that it will be useful,
+      █  but WITHOUT ANY WARRANTY; without even the implied warranty of
+      █  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      █  GNU Affero General Public License for more details.
+      █
+      █  You should have received a copy of the GNU Affero General Public License
+      █  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+      █
+      ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  ▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀██
+                                                                                                   ██
+                                                                                               ▀█▄ ██ ▄█▀
+                                                                                                 ▀████▀
+                                                                                                   ▀▀                            */
 
 namespace OpenIIoT.Core.Service.WebApi
 {
+    using OpenIIoT.SDK.Service.WebApi;
+
+    /// <summary>
+    ///     Web Routes for the <see cref="WebApiService"/>.
+    /// </summary>
     public class Routes : IRoutes
     {
         #region Public Constructors
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Routes"/> class using the specified <paramref name="configuration"/>.
+        /// </summary>
+        /// <param name="configuration">
+        ///     The configuration for the <see cref="WebApiService"/> containing user-defined route information.
+        /// </param>
         public Routes(WebApiServiceConfiguration configuration)
         {
             Root = configuration.Root.TrimStart('/').TrimEnd('/');
@@ -25,24 +70,41 @@ namespace OpenIIoT.Core.Service.WebApi
 
         #endregion Public Constructors
 
-        #region Private Properties
-
-        public string HelpRoot { get; set; }
-
-        #endregion Private Properties
-
         #region Public Properties
 
+        /// <summary>
+        ///     Gets the base route for Api endpoints.
+        /// </summary>
         public string Api { get; private set; }
 
+        /// <summary>
+        ///     Gets the user-friendly route for SwaggerUi.
+        /// </summary>
         public string Help { get; private set; }
 
+        /// <summary>
+        ///     Gets the base route for Swagger components.
+        /// </summary>
+        public string HelpRoot { get; private set; }
+
+        /// <summary>
+        ///     Gets the user-defined web root.
+        /// </summary>
         public string Root { get; private set; }
 
+        /// <summary>
+        ///     Gets the route for SignalR.
+        /// </summary>
         public string SignalR { get; private set; }
 
+        /// <summary>
+        ///     Gets the route for the Swagger json file.
+        /// </summary>
         public string Swagger { get; private set; }
 
+        /// <summary>
+        ///     Gets the route for SwaggerUi.
+        /// </summary>
         public string SwaggerUi { get; private set; }
 
         #endregion Public Properties
--- OpenIIoT.SDK/Service/WebApi/IRoutes.cs
+++ OpenIIoT.SDK/Service/WebApi/IRoutes.cs
@@ -1,18 +1,86 @@
-﻿namespace OpenIIoT.SDK.Service.WebApi
+﻿/*
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀  ▀  ▀      ▀▀
+      █
+      █    ▄█     ▄████████
+      █   ███    ███    ███
+      █   ███▌  ▄███▄▄▄▄██▀  ██████  ██   █      ██       ▄█████   ▄█████
+      █   ███▌ ▀▀███▀▀▀▀▀   ██    ██ ██   ██ ▀███████▄   ██   █    ██  ▀
+      █   ███▌ ▀███████████ ██    ██ ██   ██     ██  ▀  ▄██▄▄      ██
+      █   ███    ███    ███ ██    ██ ██   ██     ██    ▀▀██▀▀    ▀███████
+      █   ███    ███    ███ ██    ██ ██   ██     ██      ██   █     ▄  ██
+      █   █▀     ███    ███  ██████  ██████     ▄██▀     ███████  ▄████▀
+      █
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄  ▄▄ ▄▄   ▄▄▄▄ ▄▄     ▄▄     ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄ ▄
+ █████████████████████████████████████████████████████████████ ███████████████ ██  ██ ██   ████ ██     ██     ████████████████ █ █
+      ▄
+      █  Web Routes for the Web Service.
+      █
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀ ▀ ▀▀▀     ▀▀               ▀
+      █  The GNU Affero General Public License (GNU AGPL)
+      █
+      █  Copyright (C) 2016-2017 JP Dillingham (jp@dillingham.ws)
+      █
+      █  This program is free software: you can redistribute it and/or modify
+      █  it under the terms of the GNU Affero General Public License as published by
+      █  the Free Software Foundation, either version 3 of the License, or
+      █  (at your option) any later version.
+      █
+      █  This program is distributed in the hope that it will be useful,
+      █  but WITHOUT ANY WARRANTY; without even the implied warranty of
+      █  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      █  GNU Affero General Public License for more details.
+      █
+      █  You should have received a copy of the GNU Affero General Public License
+      █  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+      █
+      ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  ▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀██
+                                                                                                   ██
+                                                                                               ▀█▄ ██ ▄█▀
+                                                                                                 ▀████▀
+                                                                                                   ▀▀                            */
+
+namespace OpenIIoT.SDK.Service.WebApi
 {
+    /// <summary>
+    ///     Web Routes for the <see cref="IWebApiService"/>.
+    /// </summary>
     public interface IRoutes
     {
         #region Public Properties
 
+        /// <summary>
+        ///     Gets the base route for Api endpoints.
+        /// </summary>
         string Api { get; }
+
+        /// <summary>
+        ///     Gets the user-friendly route for SwaggerUi.
+        /// </summary>
         string Help { get; }
+
+        /// <summary>
+        ///     Gets the base route for Swagger components.
+        /// </summary>
         string HelpRoot { get; }
+
+        /// <summary>
+        ///     Gets the user-defined web root.
+        /// </summary>
         string Root { get; }
 
+        /// <summary>
+        ///     Gets the route for SignalR.
+        /// </summary>
         string SignalR { get; }
 
+        /// <summary>
+        ///     Gets the route for the Swagger json file.
+        /// </summary>
         string Swagger { get; }
 
+        /// <summary>
+        ///     Gets the route for SwaggerUi.
+        /// </summary>
         string SwaggerUi { get; }
 
         #endregion Public Properties
