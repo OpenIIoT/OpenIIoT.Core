5502a16a8e932172e87ef5e6b79d7f665ac564fa
JP Dillingham <jpdillingham@gmail.com>
2017-10-25 19:29:18 -0500

    Added documentation to I/PackageScanner

--- OpenIIoT.Core/Packaging/IPackageScanner.cs
+++ OpenIIoT.Core/Packaging/IPackageScanner.cs
@@ -1,15 +1,72 @@
-﻿namespace OpenIIoT.Core.Packaging
+﻿/*
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀  ▀  ▀      ▀▀
+      █
+      █    ▄█     ▄███████▄                                                                 ▄████████
+      █   ███    ███    ███                                                                ███    ███
+      █   ███▌   ███    ███   ▄█████   ▄██████    █  █▄     ▄█████     ▄████▄     ▄█████   ███    █▀   ▄██████   ▄█████  ██▄▄▄▄  ██▄▄▄▄     ▄█████    █████
+      █   ███▌   ███    ███   ██   ██ ██    ██   ██ ▄██▀    ██   ██   ██    ▀    ██   █    ███        ██    ██   ██   ██ ██▀▀▀█▄ ██▀▀▀█▄   ██   █    ██  ██
+      █   ███▌ ▀█████████▀    ██   ██ ██    ▀    ██▐█▀      ██   ██  ▄██        ▄██▄▄    ▀███████████ ██    ▀    ██   ██ ██   ██ ██   ██  ▄██▄▄     ▄██▄▄█▀
+      █   ███    ███        ▀████████ ██    ▄  ▀▀████     ▀████████ ▀▀██ ███▄  ▀▀██▀▀             ███ ██    ▄  ▀████████ ██   ██ ██   ██ ▀▀██▀▀    ▀███████
+      █   ███    ███          ██   ██ ██    ██   ██ ▀██▄    ██   ██   ██    ██   ██   █     ▄█    ███ ██    ██   ██   ██ ██   ██ ██   ██   ██   █    ██  ██
+      █   █▀    ▄████▀        ██   █▀ ██████▀    ▀█   ▀█▀   ██   █▀   ██████▀    ███████  ▄████████▀  ██████▀    ██   █▀  █   █   █   █    ███████   ██  ██
+      █
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄  ▄▄ ▄▄   ▄▄▄▄ ▄▄     ▄▄     ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄ ▄
+ █████████████████████████████████████████████████████████████ ███████████████ ██  ██ ██   ████ ██     ██     ████████████████ █ █
+      ▄
+      █  Scans a given directory for IPackage and IPackageArchive instances.
+      █
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀ ▀ ▀▀▀     ▀▀               ▀
+      █  The GNU Affero General Public License (GNU AGPL)
+      █
+      █  Copyright (C) 2016-2017 JP Dillingham (jp@dillingham.ws)
+      █
+      █  This program is free software: you can redistribute it and/or modify
+      █  it under the terms of the GNU Affero General Public License as published by
+      █  the Free Software Foundation, either version 3 of the License, or
+      █  (at your option) any later version.
+      █
+      █  This program is distributed in the hope that it will be useful,
+      █  but WITHOUT ANY WARRANTY; without even the implied warranty of
+      █  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      █  GNU Affero General Public License for more details.
+      █
+      █  You should have received a copy of the GNU Affero General Public License
+      █  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+      █
+      ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  ▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀██
+                                                                                                   ██
+                                                                                               ▀█▄ ██ ▄█▀
+                                                                                                 ▀████▀
+                                                                                                   ▀▀                            */
+
+namespace OpenIIoT.Core.Packaging
 {
     using System.Collections.Generic;
     using OpenIIoT.SDK.Common.OperationResult;
     using OpenIIoT.SDK.Packaging;
 
+    /// <summary>
+    ///     Scans a given directory for <see cref="IPackage"/> and <see cref="IPackageArchive"/> instances.
+    /// </summary>
     public interface IPackageScanner
     {
         #region Public Methods
 
+        /// <summary>
+        ///     Scans the directory specified in <see cref="Platform.Directories.PackageArchives"/> for
+        ///     <see cref="IPackageArchive"/> s.
+        /// </summary>
+        /// <returns>
+        ///     A Result containing the result of the operation and a <see cref="List{T}"/> of found <see cref="IPackageArchive"/> s.
+        /// </returns>
         IResult<IList<IPackageArchive>> ScanPackageArchives();
 
+        /// <summary>
+        ///     Scans the directory specified in <see cref="Platform.Directories.Packages"/> for <see cref="IPackage"/> s.
+        /// </summary>
+        /// <returns>
+        ///     A Result containing the result of the operation and a <see cref="List{T}"/> of found <see cref="IPackage"/> s.
+        /// </returns>
         IResult<IList<IPackage>> ScanPackages();
 
         #endregion Public Methods
--- OpenIIoT.Core/Packaging/PackageScanner.cs
+++ OpenIIoT.Core/Packaging/PackageScanner.cs
@@ -1,14 +1,56 @@
-﻿namespace OpenIIoT.Core.Packaging
+﻿/*
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀  ▀  ▀      ▀▀
+      █
+      █      ▄███████▄                                                                 ▄████████
+      █     ███    ███                                                                ███    ███
+      █     ███    ███   ▄█████   ▄██████    █  █▄     ▄█████     ▄████▄     ▄█████   ███    █▀   ▄██████   ▄█████  ██▄▄▄▄  ██▄▄▄▄     ▄█████    █████
+      █     ███    ███   ██   ██ ██    ██   ██ ▄██▀    ██   ██   ██    ▀    ██   █    ███        ██    ██   ██   ██ ██▀▀▀█▄ ██▀▀▀█▄   ██   █    ██  ██
+      █   ▀█████████▀    ██   ██ ██    ▀    ██▐█▀      ██   ██  ▄██        ▄██▄▄    ▀███████████ ██    ▀    ██   ██ ██   ██ ██   ██  ▄██▄▄     ▄██▄▄█▀
+      █     ███        ▀████████ ██    ▄  ▀▀████     ▀████████ ▀▀██ ███▄  ▀▀██▀▀             ███ ██    ▄  ▀████████ ██   ██ ██   ██ ▀▀██▀▀    ▀███████
+      █     ███          ██   ██ ██    ██   ██ ▀██▄    ██   ██   ██    ██   ██   █     ▄█    ███ ██    ██   ██   ██ ██   ██ ██   ██   ██   █    ██  ██
+      █    ▄████▀        ██   █▀ ██████▀    ▀█   ▀█▀   ██   █▀   ██████▀    ███████  ▄████████▀  ██████▀    ██   █▀  █   █   █   █    ███████   ██  ██
+      █
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄  ▄▄ ▄▄   ▄▄▄▄ ▄▄     ▄▄     ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄ ▄
+ █████████████████████████████████████████████████████████████ ███████████████ ██  ██ ██   ████ ██     ██     ████████████████ █ █
+      ▄
+      █  Scans a given directory for IPackage and IPackageArchive instances.
+      █
+      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀ ▀ ▀▀▀     ▀▀               ▀
+      █  The GNU Affero General Public License (GNU AGPL)
+      █
+      █  Copyright (C) 2016-2017 JP Dillingham (jp@dillingham.ws)
+      █
+      █  This program is free software: you can redistribute it and/or modify
+      █  it under the terms of the GNU Affero General Public License as published by
+      █  the Free Software Foundation, either version 3 of the License, or
+      █  (at your option) any later version.
+      █
+      █  This program is distributed in the hope that it will be useful,
+      █  but WITHOUT ANY WARRANTY; without even the implied warranty of
+      █  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      █  GNU Affero General Public License for more details.
+      █
+      █  You should have received a copy of the GNU Affero General Public License
+      █  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+      █
+      ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  ▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀██
+                                                                                                   ██
+                                                                                               ▀█▄ ██ ▄█▀
+                                                                                                 ▀████▀
+                                                                                                   ▀▀                            */
+
+namespace OpenIIoT.Core.Packaging
 {
     using System;
     using System.Collections.Generic;
     using NLog.xLogger;
     using OpenIIoT.SDK.Common.OperationResult;
     using OpenIIoT.SDK.Packaging;
-    using OpenIIoT.SDK.Packaging.Operations;
     using OpenIIoT.SDK.Platform;
-    using System.IO;
 
+    /// <summary>
+    ///     Scans a given directory for <see cref="IPackage"/> and <see cref="IPackageArchive"/> instances.
+    /// </summary>
     public class PackageScanner : IPackageScanner
     {
         #region Private Fields
@@ -16,22 +58,29 @@
         /// <summary>
         ///     The Logger for this class.
         /// </summary>
-        private static new xLogger logger = xLogManager.GetCurrentClassxLogger();
+        private static xLogger logger = xLogManager.GetCurrentClassxLogger();
 
         #endregion Private Fields
 
         #region Public Constructors
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PackageScanner"/> class.
+        /// </summary>
         public PackageScanner()
             : this(ApplicationManager.GetInstance().GetManager<IPlatformManager>(), new PackageFactory())
         {
         }
 
-        public PackageScanner(IPackageFactory packageFactory)
-            : this(ApplicationManager.GetInstance().GetManager<IPlatformManager>(), new PackageFactory())
-        {
-        }
-
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PackageScanner"/> class with the specified
+        ///     <paramref name="platformManager"/> and <paramref name="packageFactory"/>.
+        /// </summary>
+        /// <param name="platformManager">The <see cref="IPlatformManager"/> instance for the application.</param>
+        /// <param name="packageFactory">
+        ///     The <see cref="IPackageFactory"/> used to create <see cref="IPackage"/> and <see cref="IPackageArchive"/> instances
+        ///     from files and directories found by the scanner.
+        /// </param>
         public PackageScanner(IPlatformManager platformManager, IPackageFactory packageFactory)
         {
             PlatformManager = platformManager;
@@ -42,12 +91,19 @@
 
         #region Private Properties
 
+        /// <summary>
+        ///     Gets the current <see cref="IPlatform"/> instance.
+        /// </summary>
         private IPlatform Platform => PlatformManager.Platform;
 
+        /// <summary>
+        ///     Gets or sets the <see cref="IPackageFactory"/> used to create <see cref="IPackage"/> and
+        ///     <see cref="IPackageArchive"/> instances from files and directories found by the scanner.
+        /// </summary>
         private IPackageFactory PackageFactory { get; set; }
 
         /// <summary>
-        ///     Gets the <see cref="IPlatformManager"/> with which Platform operations are carried out.
+        ///     Gets or sets the <see cref="IPlatformManager"/> with which Platform operations are carried out.
         /// </summary>
         private IPlatformManager PlatformManager { get; set; }
 
@@ -101,6 +157,12 @@
             return retVal;
         }
 
+        /// <summary>
+        ///     Scans the directory specified in <see cref="Platform.Directories.Packages"/> for <see cref="IPackage"/> s.
+        /// </summary>
+        /// <returns>
+        ///     A Result containing the result of the operation and a <see cref="List{T}"/> of found <see cref="IPackage"/> s.
+        /// </returns>
         public IResult<IList<IPackage>> ScanPackages()
         {
             Guid guid = logger.EnterMethod(true);
