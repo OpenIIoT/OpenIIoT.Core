7a7387463b756a420c4a61f87fc937ab9d825fcd
JP Dillingham <jpdillingham@gmail.com>
2017-11-05 21:54:15 -0600

    Completed Package tests

--- OpenIIoT.Core.Tests/Packaging/PackageTests.cs
+++ OpenIIoT.Core.Tests/Packaging/PackageTests.cs
@@ -50,6 +50,9 @@
 
 namespace OpenIIoT.Core.Tests.Packaging
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
     using Moq;
     using OpenIIoT.Core.Packaging;
     using OpenIIoT.SDK.Packaging.Manifest;
@@ -58,18 +61,96 @@ namespace OpenIIoT.Core.Tests.Packaging
     /// <summary>
     ///     Unit tests for the <see cref="Package"/> class.
     /// </summary>
-    public class PackageTests
+    public class PackageTests : IDisposable
     {
+        #region Public Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PackageTests"/> class.
+        /// </summary>
+        public PackageTests()
+        {
+            ManifestMock = new Mock<IPackageManifest>();
+
+            SetupMocks();
+
+            Temp = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(Temp);
+
+            File.WriteAllText(Path.Combine(Temp, "test.txt"), string.Empty);
+        }
+
+        #endregion Public Constructors
+
         #region Private Properties
 
+        /// <summary>
+        ///     Gets or sets the IPackageManifest mockup.
+        /// </summary>
         private Mock<IPackageManifest> ManifestMock { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the temporary test directory.
+        /// </summary>
+        private string Temp { get; set; }
 
         #endregion Private Properties
 
+        #region Public Methods
+
+        /// <summary>
+        ///     Tests the constructor and all properties.
+        /// </summary>
+        [Fact]
+        public void Constructor()
+        {
+            DirectoryInfo dir = new DirectoryInfo(Temp);
+
+            Package test = new Package(dir, ManifestMock.Object);
+
+            Assert.IsType<Package>(test);
+            Assert.NotNull(test);
+
+            Assert.Equal(Temp, test.DirectoryName);
+            Assert.Equal(new List<string>(new[] { Path.Combine(Temp, "test.txt") }), test.Files);
+            Assert.Equal("OpenIIoT.Tests.Test", test.FQN);
+            Assert.Equal(dir.CreationTimeUtc, test.InstalledOn);
+            Assert.Equal(ManifestMock.Object, test.Manifest);
+        }
+
+        /// <summary>
+        ///     Disposes of this instance.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion Public Methods
+
+        #region Protected Methods
+
+        /// <summary>
+        ///     Disposes of this instance.
+        /// </summary>
+        /// <param name="disposing">A value indicating whether this method has been called directly or indirectly from code.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            Directory.Delete(Temp, true);
+        }
+
+        #endregion Protected Methods
+
         #region Private Methods
 
+        /// <summary>
+        ///     Configures the mockups for the unit tests.
+        /// </summary>
         private void SetupMocks()
         {
+            ManifestMock.Setup(m => m.Name).Returns("Test");
+            ManifestMock.Setup(m => m.Namespace).Returns("OpenIIoT.Tests");
         }
 
         #endregion Private Methods
